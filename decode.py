f9416b028578af7dd6f62c0050855f4b5375777f
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2010-12-07 15:35:11 +0100

    decode now also with midi

--- decode.py
+++ decode.py
@@ -5,43 +5,59 @@ from subprocess import Popen, PIPE
 import os
 from struct import *
 
-sample_input_file = "/Users/az/Music/Classic/Glenn Gould Plays Bach/French Suites, BWV812-7 - Gould/Bach, French Suite 1 in d, BWV812 - 1 Allemande.mp3"
+sample_mp3_file = "/Users/az/Music/Classic/Glenn Gould Plays Bach/French Suites, BWV812-7 - Gould/Bach, French Suite 1 in d, BWV812 - 1 Allemande.mp3"
+sample_mid_file = "/Users/az/Programmierung/MidiWriter/mf_BachPrelude2.mid"
 
-def pcm_stream(filename):
+
+def ffmpeg_to_wav(stream):
 	sampleRate = 44100
 	
 	p = Popen([
 		"ffmpeg",
-		"-i", filename,
+		"-i", "-",
 		"-vn", "-acodec", "pcm_s16le",
 		"-ac", "1", "-ar", str(sampleRate),
 		"-f", "wav", "-"],
-		stdin = open(os.devnull), stdout = PIPE, stderr = open(os.devnull, "w"))
+		stdin = stream, stdout = PIPE, stderr = open(os.devnull, "w"))
+	return p.stdout
 	
-	h_chunkid, h_chunksize, h_rifftype = unpack("<4sI4s", p.stdout.read(12))
+def midi_to_wav(stream):
+	p = Popen([
+		"timidity", "-",
+		"-A90", "-a",
+		"-Ow", "--output-mono", "--output-signed", "--output-16bit",
+		"-o", "-"],
+		stdin = stream, stdout = PIPE, stderr = open(os.devnull, "w"))	
+	return p.stdout
+	
+def wav_to_rawpcm(stream):
+	sampleRate = 44100
+	
+	h_chunkid, h_chunksize, h_rifftype = unpack("<4sI4s", stream.read(12))
 	if h_chunkid != "RIFF":
 		raise Exception, "stream is not in RIFF format"
 	if h_rifftype != "WAVE":
 		raise Exception, "stream is not in WAVE format"
 		
-	if p.stdout.read(4) != "fmt ": raise Exception, "fmt section expected"
-	if unpack("<L", p.stdout.read(4))[0] != 16: raise Exception, "fmt section should be of size 16"
-	wFormatTag = unpack("<H", p.stdout.read(2))[0]
+	if stream.read(4) != "fmt ": raise Exception, "fmt section expected"
+	if unpack("<L", stream.read(4))[0] != 16: raise Exception, "fmt section should be of size 16"
+	wFormatTag = unpack("<H", stream.read(2))[0]
 	if wFormatTag != 1: raise Exception, "PCM format expected but got " + hex(wFormatTag)
-	wChannels = unpack("<H", p.stdout.read(2))[0]
-	dwSamplesPerSec = unpack("<L", p.stdout.read(4))[0]
-	dwAvgBytesPerSec = unpack("<L", p.stdout.read(4))[0]
-	wBlockAlign = unpack("<H", p.stdout.read(2))[0]
-	wBitsPerSample = unpack("<H", p.stdout.read(2))[0]
+	wChannels = unpack("<H", stream.read(2))[0]
+	dwSamplesPerSec = unpack("<L", stream.read(4))[0]
+	dwAvgBytesPerSec = unpack("<L", stream.read(4))[0]
+	wBlockAlign = unpack("<H", stream.read(2))[0]
+	wBitsPerSample = unpack("<H", stream.read(2))[0]
 	
 	if dwSamplesPerSec != sampleRate: raise Exception, "we expected " + str(sampleRate) + " Hz but got " + str(dwSamplesPerSec)
 	if wBitsPerSample != 16: raise Exception, "we expected 16 bits per sample but got " + str(wBitsPerSample)
 	if wChannels != 1: raise Exception, "we expected 1 channel but got " + str(wChannels)
+	if wBlockAlign != 2: raise Exception, "we expected block align 2 but got " + str(wBlockAlign)
 	
-	if p.stdout.read(4) != "data": raise Exception, "data section expected"
-	datalen = unpack("<L", p.stdout.read(4))[0]
+	if stream.read(4) != "data": raise Exception, "data section expected"
+	datalen = unpack("<L", stream.read(4))[0]
 
 	# and now the data begins ...
-	return p.stdout, wBlockAlign
+	return stream
 
 	
