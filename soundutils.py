d90b65fd6cdd0743502109a45bee899fac453ce5
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2010-12-08 01:19:39 +0100

    sound utils

---
+++ soundutils.py
@@ -0,0 +1,31 @@
+import decode
+from cStringIO import StringIO
+import numpy
+from array import array
+
+def streamcopy(stream):
+	#return stream
+	s = StringIO()
+	c = 0
+	while True:
+		buf = stream.read(2048)
+		if len(buf) == 0: break
+		s.write(buf)
+		c += len(buf)
+	print "read", c, "bytes"
+	return s
+
+
+def arrayFromPCMStream(str, n):
+	data = array('h')
+	oldpos = str.tell()
+	data.fromstring(str.read(n * 2))
+	str.seek(oldpos)
+	data = numpy.array(data, numpy.int16)
+	return data
+	
+def calcDiff(str1, str2, dt):
+	numsamples = int(decode.sampleRate * dt)
+	diffdata = arrayFromPCMStream(str1, numsamples) - arrayFromPCMStream(str2, numsamples)
+	return numpy.linalg.norm(diffdata)
+
