4b70d3f6cd04ade49421e9d0ccb5d9f8094d709f
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-05-06 13:52:54 +0200

    small fix for new code

--- train.py
+++ train.py
@@ -4,8 +4,8 @@ import midi
 from decode import *
 from soundutils import *
 
-midistr = streamcopy(wav_to_rawpcm(midi_to_wav(open(sample_mid_file))))
-mp3str = streamcopy(wav_to_rawpcm(ffmpeg_to_wav(open(sample_mp3_file))))
+midistr = streamcopy(midi_to_rawpcm(open(sample_mid_file)))
+mp3str = streamcopy(ffmpeg_to_rawpcm(open(sample_mp3_file)))
 
 print "loaded data"
 print calcDiff(midistr, mp3str, 10)
