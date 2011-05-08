9dcf86f214f6bbbb6b92678b6adf705404e1e772
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-05-08 17:09:00 +0200

    simpler debugging

--- midi.py
+++ midi.py
@@ -450,7 +450,11 @@ def main(argv):
             printflag = 1
         elif option == '-d':
             debugflag = 1
-
+    if len(argv) <= 1:
+        import decode
+        infile = decode.sample_mid_file
+        printflag = 1
+	
     m = MidiFile()
     m.open(infile)
     m.read()
@@ -465,4 +469,4 @@ def main(argv):
 
 if __name__ == "__main__":
     main(sys.argv)
-	
+
