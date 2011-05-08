5af84df8da34968a5deae6378f6aa33169cf8a3b
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-05-08 15:50:12 +0200

    use better_exchook

--- train.py
+++ train.py
@@ -4,6 +4,9 @@ import midi
 from decode import *
 from soundutils import *
 
+from better_exchook import *
+sys.excepthook = better_exchook
+
 midistr = streamcopy(midi_to_rawpcm(open(sample_mid_file)))
 mp3str = streamcopy(ffmpeg_to_rawpcm(open(sample_mp3_file)))
 
