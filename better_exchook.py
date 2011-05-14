974bca662ebac0afdd746f7d239c7f940760ec00
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-05-15 00:08:32 +0200

    updated better_exchook

--- better_exchook.py
+++ better_exchook.py
@@ -81,9 +81,27 @@ def grep_full_py_identifiers(tokens):
 		yield token
 
 
+def output_limit():
+	return 300
+	
+def output(s):
+	limit = output_limit()
+	if len(s) > limit:
+		s = s[:limit - 3] + "..."
+	sys.stderr.write(s)
+	sys.stderr.write("\n")
+	sys.stderr.flush()
+
+def debug_shell(user_ns, user_global_ns):
+	from IPython.Shell import IPShellEmbed,IPShell
+	ipshell = IPShell(argv=[], user_ns=user_ns, user_global_ns=user_global_ns)
+	#ipshell()
+	ipshell.mainloop()
+	
+
 def better_exchook(etype, value, tb):
-	print >>sys.stderr, "EXCEPTION"
-	print >>sys.stderr, 'Traceback (most recent call last):'
+	output("EXCEPTION")
+	output('Traceback (most recent call last):')
 	topFrameLocals,topFrameGlobals = None,None
 	try:
 		import linecache
@@ -108,35 +126,34 @@ def better_exchook(etype, value, tb):
 			co = f.f_code
 			filename = co.co_filename
 			name = co.co_name
-			print >>sys.stderr, '  File "%s", line %d, in %s' % (filename,lineno,name)
+			output('  File "%s", line %d, in %s' % (filename,lineno,name))
 			linecache.checkcache(filename)
 			line = linecache.getline(filename, lineno, f.f_globals)
 			if line:
 				line = line.strip()
-				print >>sys.stderr, '    line:', line
-				print >>sys.stderr, '    locals:'
+				output('    line: ' + line)
+				output('    locals:')
 				alreadyPrintedLocals = set()
 				for tokenstr in grep_full_py_identifiers(parse_py_statement(line)):
 					splittedtoken = tuple(tokenstr.split("."))
 					for token in map(lambda i: splittedtoken[0:i], range(1, len(splittedtoken) + 1)):
 						if token in alreadyPrintedLocals: continue
-						print >>sys.stderr, '     ', ".".join(token), "=",
 						tokenvalue = None
 						tokenvalue = _trySet(tokenvalue, lambda: "<local> " + repr(_resolveIdentifier(f.f_locals, token)))
 						tokenvalue = _trySet(tokenvalue, lambda: "<global> " + repr(_resolveIdentifier(f.f_globals, token)))
 						tokenvalue = _trySet(tokenvalue, lambda: "<builtin> " + repr(_resolveIdentifier(f.f_builtins, token)))
 						tokenvalue = tokenvalue or "<not found>"
-						print >>sys.stderr, tokenvalue
+						output('      ' + ".".join(token) + " = " + tokenvalue)
 						alreadyPrintedLocals.add(token)
-				if len(alreadyPrintedLocals) == 0: print "       no locals"
+				if len(alreadyPrintedLocals) == 0: output("       no locals")
 			_tb = _tb.tb_next
 			n += 1
 
 	except Exception, e:
-		print >>sys.stderr, "ERROR: cannot get more detailed exception info because:"
+		output("ERROR: cannot get more detailed exception info because:")
 		import traceback
-		for l in traceback.format_exc().split("\n"): print >>sys.stderr, "  ", l
-		print >>sys.stderr, "simple traceback:"
+		for l in traceback.format_exc().split("\n"): output("   " + l)
+		output("simple traceback:")
 		traceback.print_tb(tb)
 
 	import types
@@ -153,9 +170,9 @@ def better_exchook(etype, value, tb):
 	if (isinstance(etype, BaseException) or
 		isinstance(etype, types.InstanceType) or
 		etype is None or type(etype) is str):
-		print >>sys.stderr, _format_final_exc_line(etype, value)
+		output(_format_final_exc_line(etype, value))
 	else:
-		print >>sys.stderr, _format_final_exc_line(etype.__name__, value)
+		output(_format_final_exc_line(etype.__name__, value))
 
 	debug = False
 	try:
@@ -163,11 +180,12 @@ def better_exchook(etype, value, tb):
 		debug = int(os.environ["DEBUG"]) != 0
 	except: pass
 	if debug:
-		from IPython.Shell import IPShellEmbed,IPShell
-		ipshell = IPShell(user_ns=topFrameLocals, user_global_ns=topFrameGlobals)
-		#ipshell()
-		ipshell.mainloop()
-
+		output("---------- DEBUG SHELL -----------")
+		debug_shell(user_ns=topFrameLocals, user_global_ns=topFrameGlobals)
+		
+def install():
+	sys.excepthook = better_exchook
+	
 if __name__ == "__main__":
 	# some examples
 	# this code produces this output: https://gist.github.com/922622
