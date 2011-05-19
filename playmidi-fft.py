29aee46b787a48d31ee59bc232c918c5571bf89c
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-05-19 15:26:17 +0200

    generic resample function

--- playmidi-fft.py
+++ playmidi-fft.py
@@ -20,6 +20,7 @@ print "done"
 
 from numpy.fft import rfft
 import numpy as np
+import math
 
 RATE = 44100
 N_window = RATE / 10
@@ -39,18 +40,41 @@ def show_which_freq(freqs):
 		thefreq = which*RATE/N_window
 		print "The freq is %f Hz." % (thefreq)
 
+def resample(data, outdim):
+	indim = len(data)
+	outdata = [0.0] * outdim
+	outentrylen = indim * 1.0 / outdim
+	for outidx in xrange(outdim):
+		x = 0.0
+		startinidx = outidx * indim * 1.0 / outdim
+		endinidx = (outidx + 1) * indim * 1.0 / outdim
+		if not startinidx.is_integer():
+			x += (1.0 - math.fmod(startinidx, 1)) * data[int(math.floor(startinidx))]
+			inidx = int(math.ceil(startinidx))
+		else:
+			inidx = int(startinidx)
+		while inidx < math.floor(endinidx):
+			x += data[inidx]
+			inidx += 1
+		if not endinidx.is_integer() and int(math.floor(endinidx)) < len(data):
+			x += math.fmod(endinidx, 1) * data[int(math.floor(endinidx))]
+		x /= outentrylen
+		outdata[outidx] = x
+	return outdata
 
-while rawpcm.tell() < len(rawpcm.getvalue()):
-	data = arrayFromPCMStream(rawpcm, N_window/10)
-	fdata += list(data)
-	if len(fdata) > N_window:
-		fdata = fdata[len(fdata) - N_window:]
+if __name__ == '__main__':
+
+	while rawpcm.tell() < len(rawpcm.getvalue()):
+		data = arrayFromPCMStream(rawpcm, N_window/10)
+		fdata += list(data)
+		if len(fdata) > N_window:
+			fdata = fdata[len(fdata) - N_window:]
+			
+			freqs = rfft(window * fdata)
+			freqs = abs(freqs) ** 2
+			show_which_freqs(freqs)
 		
-		freqs = rfft(window * fdata)
-		freqs = abs(freqs) ** 2
-		show_which_freqs(freqs)
+		play(data)
 	
-	play(data)
-
-print
-print "finished"
+	print
+	print "finished"
