ab43c570958fd618e550bb58bee8198f36221527
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-06-15 23:51:00 +0200

    working version. playing around with SDL keyboard events

--- keyboard-player.py
+++ keyboard-player.py
@@ -1,22 +1,103 @@
 #!/usr/bin/python
 
-import sys
+import sys, os
+from ctypes import *	
+sdl = None
 
-from Foundation import NSAutoreleasePool
-pool = NSAutoreleasePool.alloc().init()
+def app_main():
+	sdl.SDL_Init.argtypes = (c_uint32,)
+	print "SDL_Init:", sdl.SDL_Init(0xFFFF) # init everything
+	
+	c_SDLSurface_p = c_void_p # this suffice for us
+	sdl.SDL_SetVideoMode.restype = c_SDLSurface_p # screen
+	sdl.SDL_SetVideoMode.argtypes = (c_int, c_int, c_int, c_uint32) # width,height,bpp,flags
+	
+	screenSurface = sdl.SDL_SetVideoMode(640,480,0,0)
 
-from ctypes import *
+	c_SDLKey = c_uint32
+	c_SDLMod = c_uint32
+	
+	class c_SDLkeysym(Structure):
+		_fields_ = [
+			("scancode", c_uint8),
+			("sym", c_SDLKey),
+			("mod", c_SDLMod),
+			("unicode", c_uint16)
+			]
+	
+	class c_SDLKeyboardEvent(Structure):
+		_fields_ = [
+			("type", c_uint8),
+			("which", c_uint8), # which keyboard device
+			("state", c_uint8), # 1 - down, 0 - up
+			("keysym", c_SDLkeysym)
+			]
+	
+	class c_SDLDummyEvent(Structure):
+		_fields_ = [
+			("type", c_uint8),
+			("data", c_uint8*20) # just some space filler so that we are big enough
+			]
+	
+	class c_SDLEvent(Union):
+		_fields_ = [
+			("type", c_uint8),
+			("key", c_SDLKeyboardEvent),
+			("dummy", c_SDLDummyEvent)
+			]
+	
+	class SDLEventTypes:
+		# just the ones we need for now
+		SDL_KEYDOWN = 2
+		SDL_KEYUP = 3
+		SDL_QUIT = 12
+		
+	ev = c_SDLEvent()
+	
+	sdl.SDL_EnableUNICODE(1)
+	
+	sdl.SDL_WaitEvent.argtypes = (POINTER(c_SDLEvent),)
+	while sdl.SDL_WaitEvent(pointer(ev)) == 1:
+		if ev.type == SDLEventTypes.SDL_QUIT: break
+		elif ev.type in [SDLEventTypes.SDL_KEYDOWN, SDLEventTypes.SDL_KEYUP]:
+			print "SDL keyboard event:", ev.key.state, ev.key.keysym.sym, ev.key.keysym.unicode
+	
+	# foo
+	
+	sdl.SDL_Quit.restype = None
+	sdl.SDL_Quit()
+	os._exit(0)
 
-sdl = cdll.LoadLibrary("/Library/Frameworks/SDL.framework/SDL")
+if sys.platform == "darwin":
 
-sdl.SDL_Init.argtypes = (c_uint32,)
-print "SDL_Init:", sdl.SDL_Init(0xFFFF) # init everything
+	sdl = cdll.LoadLibrary("/Library/Frameworks/SDL.framework/SDL")
 
+	from AppKit import NSApp, NSApplication, NSNotificationCenter, NSApplicationDidFinishLaunchingNotification
+	from Foundation import NSAutoreleasePool, NSObject
+	pool = NSAutoreleasePool.alloc().init()
+	
+	class MyApplicationActivator(NSObject):
+	
+		def activateNow_(self, aNotification):
+			NSApp().activateIgnoringOtherApps_(True)
+			app_main()
 
+	activator = MyApplicationActivator.alloc().init()
+	NSNotificationCenter.defaultCenter().addObserver_selector_name_object_(
+		activator,
+		'activateNow:',
+		NSApplicationDidFinishLaunchingNotification,
+		None,
+	)
 
-# foo
+	import PyObjCTools.AppHelper
+	#PyObjCTools.AppHelper.runEventLoop()
+	
+	NSApplication.sharedApplication()
+	NSApp().run()
+	
+	del pool
 
-sdl.SDL_Quit.restype = None
-sdl.SDL_Quit()
-
-del pool
+else:
+	app_main()
+	
