a2aac0b452c1fe44db2f59ad5151bcfd67af1141
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-06-16 11:51:03 +0200

    some restructuring

--- keyboard-player.py
+++ keyboard-player.py
@@ -7,57 +7,59 @@ import sys, os
 from ctypes import *	
 sdl = None
 
-def app_main():
+c_SDLSurface_p = c_void_p # this suffice for us
+
+c_SDLKey = c_uint32
+c_SDLMod = c_uint32
+
+class c_SDLkeysym(Structure):
+	_fields_ = [
+		("scancode", c_uint8),
+		("sym", c_SDLKey),
+		("mod", c_SDLMod),
+		("unicode", c_uint16)
+		]
+
+class c_SDLKeyboardEvent(Structure):
+	_fields_ = [
+		("type", c_uint8),
+		("which", c_uint8), # which keyboard device
+		("state", c_uint8), # 1 - down, 0 - up
+		("keysym", c_SDLkeysym)
+		]
+
+class c_SDLDummyEvent(Structure):
+	_fields_ = [
+		("type", c_uint8),
+		("data", c_uint8*20) # just some space filler so that we are big enough
+		]
+
+class c_SDLEvent(Union):
+	_fields_ = [
+		("type", c_uint8),
+		("key", c_SDLKeyboardEvent),
+		("dummy", c_SDLDummyEvent)
+		]
+
+class SDLEventTypes:
+	# just the ones we need for now
+	SDL_KEYDOWN = 2
+	SDL_KEYUP = 3
+	SDL_QUIT = 12
+
+def init_SDL_dll():
 	sdl.SDL_Init.argtypes = (c_uint32,)
-	sdl.SDL_Init(0xFFFF) # init everything
-	
-	c_SDLSurface_p = c_void_p # this suffice for us
+
 	sdl.SDL_SetVideoMode.restype = c_SDLSurface_p # screen
 	sdl.SDL_SetVideoMode.argtypes = (c_int, c_int, c_int, c_uint32) # width,height,bpp,flags
 	
-	screenSurface = sdl.SDL_SetVideoMode(640,480,0,0)
+	sdl.SDL_WaitEvent.argtypes = (POINTER(c_SDLEvent),)
 
-	c_SDLKey = c_uint32
-	c_SDLMod = c_uint32
-	
-	class c_SDLkeysym(Structure):
-		_fields_ = [
-			("scancode", c_uint8),
-			("sym", c_SDLKey),
-			("mod", c_SDLMod),
-			("unicode", c_uint16)
-			]
-	
-	class c_SDLKeyboardEvent(Structure):
-		_fields_ = [
-			("type", c_uint8),
-			("which", c_uint8), # which keyboard device
-			("state", c_uint8), # 1 - down, 0 - up
-			("keysym", c_SDLkeysym)
-			]
-	
-	class c_SDLDummyEvent(Structure):
-		_fields_ = [
-			("type", c_uint8),
-			("data", c_uint8*20) # just some space filler so that we are big enough
-			]
-	
-	class c_SDLEvent(Union):
-		_fields_ = [
-			("type", c_uint8),
-			("key", c_SDLKeyboardEvent),
-			("dummy", c_SDLDummyEvent)
-			]
-	
-	class SDLEventTypes:
-		# just the ones we need for now
-		SDL_KEYDOWN = 2
-		SDL_KEYUP = 3
-		SDL_QUIT = 12
-		
+	sdl.SDL_Quit.restype = None
+
+def sdl_main_loop():
 	ev = c_SDLEvent()
 		
-	sdl.SDL_WaitEvent.argtypes = (POINTER(c_SDLEvent),)
 	while sdl.SDL_WaitEvent(pointer(ev)) == 1:
 		if ev.type == SDLEventTypes.SDL_QUIT: break
 		elif ev.type in [SDLEventTypes.SDL_KEYDOWN, SDLEventTypes.SDL_KEYUP]:
@@ -68,9 +70,13 @@ def app_main():
 			
 			if down and sym == 'q': break
 			if down and sym == '\x1b': break # ESC
-			
-			
-	sdl.SDL_Quit.restype = None
+
+def app_main():
+	init_SDL_dll()
+	
+	sdl.SDL_Init(0xFFFF) # init everything	
+	sdl.SDL_SetVideoMode(640,480,0,0)
+	sdl_main_loop()
 	sdl.SDL_Quit()
 
 if sys.platform == "darwin":
